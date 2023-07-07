8b37297fd3c549aa5a9da900f2f80c3b45010260
Nikita <nikita@oden.com>
2023-07-07 19:48:54 +0300

    Refactor the capitalize function and its test.

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,9 +1,6 @@
-export default function capitalize(text = null) {
-  if (typeof text !== "string") return null;
-  if (!isNaN(+text.charAt())) return null;
-  if (text.charAt() === text.charAt().toUpperCase()) return text;
-  return text.charAt(0).toUpperCase() + text.slice(1);
-}
-//when there is nothing to capatalise return uncepetelised not null
+export default function capitalize(str = null) {
+  if (typeof str !== "string") return null;
+  if (str.charAt(0) === str.charAt(0).toUpperCase()) return str;
 
-//ASK chat gpt everything fine
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -5,36 +5,17 @@ test("capitalize first letter", () => {
   expect(capitalize("Hello")).toBe("Hello");
 });
 
-test("if the argument is empty, returns null", () => {
-  expect(capitalize()).toBe(null);
+test("if the first symbol isn't a letter, returns the unchanged string", () => {
+  expect(capitalize("1st, 2nd, 3rd")).toBe("1st, 2nd, 3rd");
+  expect(capitalize("@#90-(30-20-10)-90#@")).toBe("@#90-(30-20-10)-90#@");
 });
 
-test("if the argument is empty, returns null", () => {
-  expect(capitalize()).toBe(null);
-});
-//-----------------------------------------
-test("if the argument is not a string, returns null", () => {
+test("if the argument isn't a string, returns null", () => {
   expect(capitalize({})).toBe(null);
   expect(capitalize([])).toBe(null);
   expect(capitalize(242342)).toBe(null);
 });
 
-test("if the argument is a string number, returns null", () => {
-  expect(capitalize('242342')).toBe(null);
+test("if the argument is empty, returns null", () => {
+  expect(capitalize()).toBe(null);
 });
-
-test("if the first symbol in the text is a digit, returns null", () => {
-  expect(capitalize('1st hello 2nd bye')).toBe(null);
-});
-
-test("if the first symbol isn't a digit, returns the text with the capitalized first letter", () => {
-  expect(capitalize('current year is 2023')).toBe('Current year is 2023');
-});
-
-test("if the first symbol is something strange, returns the text with the capitalized first letter", () => {
-  expect(capitalize('@')).toBe(null);
-});
-
-
-//when there is nothing to capatalise return uncepetelised not null
-//ASK chat gpt everything fine
