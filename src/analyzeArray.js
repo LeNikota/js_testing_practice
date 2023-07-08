99547ddf689d0a1c7e7a7e10049cfc407ad5b78c
Nikita <nikita@oden.com>
2023-07-09 02:30:52 +0300

    Refactor the analyzeArray code and its test

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,10 +1,18 @@
 export default function analyzeArray(arr) {
-  if(!Array.isArray(arr) || arr.length === 0) return null;
-  
+  if (
+    !Array.isArray(arr) ||
+    arr.length === 0 ||
+    arr.some((element) => typeof element !== "number")
+  ) {
+    return null;
+  }
+
   return {
     min: Math.min(...arr),
     max: Math.max(...arr),
-    average: arr.reduce((accumulator, current) => accumulator + current, 0) / arr.length,
+    average:
+      arr.reduce((accumulator, current) => accumulator + current, 0) /
+      arr.length,
     length: arr.length,
   };
 }
--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -7,10 +7,48 @@ test("analyzes an array, returns average, min, max, length", () => {
     average: 5.5,
     length: 10,
   });
+
   expect(analyzeArray([2, 5, 10, 20, 40])).toEqual({
     min: 2,
     max: 40,
     average: 15.4,
     length: 5,
   });
+
+  expect(analyzeArray([-10, -20, 100, -30])).toEqual({
+    min: -30,
+    max: 100,
+    average: 10,
+    length: 4,
+  });
+});
+
+test("analyzes an array with one element", () => {
+  expect(analyzeArray([5])).toEqual({
+    min: 5,
+    max: 5,
+    average: 5,
+    length: 1,
+  });
+});
+
+test("returns null for non-array inputs", () => {
+  expect(analyzeArray()).toBeNull();
+  expect(analyzeArray(null)).toBeNull();
+  expect(analyzeArray(undefined)).toBeNull();
+  expect(analyzeArray("")).toBeNull();
+  expect(analyzeArray(123)).toBeNull();
+  expect(analyzeArray({})).toBeNull();
+});
+
+test("returns null for array containing or non-numeric values", () => {
+  expect(analyzeArray([1, 2, "3", 4, 5])).toBeNull();
+  expect(analyzeArray([1, 2, null, 4, 5])).toBeNull();
+  expect(analyzeArray([1, 2, false, 4, 5])).toBeNull();
+  expect(analyzeArray([1, 2, [2,3], 4, 5])).toBeNull();
+  expect(analyzeArray([1, 2, {}, 4, 5])).toBeNull();
+});
+
+test("returns null for empty array", () => {
+  expect(analyzeArray([])).toBeNull();
 });
