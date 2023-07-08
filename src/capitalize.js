ee2772b6e1fb3d59bec3eebfea844f83883a8cb8
Nikita <nikita@oden.com>
2023-07-08 22:47:31 +0300

    Refactor the capitalize function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,5 @@
 export default function capitalize(str = null) {
-  if (typeof str !== "string") return null;
+  if (typeof str !== "string") return '';
   if (str.charAt(0) === str.charAt(0).toUpperCase()) return str;
 
   return str.charAt(0).toUpperCase() + str.slice(1);
--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -8,14 +8,22 @@ test("capitalize first letter", () => {
 test("if the first symbol isn't a letter, returns the unchanged string", () => {
   expect(capitalize("1st, 2nd, 3rd")).toBe("1st, 2nd, 3rd");
   expect(capitalize("@#90-(30-20-10)-90#@")).toBe("@#90-(30-20-10)-90#@");
+  expect(capitalize("     ")).toBe("     ");
 });
 
-test("if the argument isn't a string, returns null", () => {
-  expect(capitalize({})).toBe(null);
-  expect(capitalize([])).toBe(null);
-  expect(capitalize(242342)).toBe(null);
+test("when the string is a single character, should capitalize the character", () => {
+  expect(capitalize("h")).toBe("H");
+  expect(capitalize("H")).toBe("H");
+  expect(capitalize("1")).toBe("1");
+  expect(capitalize("@")).toBe("@");
 });
 
-test("if the argument is empty, returns null", () => {
-  expect(capitalize()).toBe(null);
+test("if the argument isn't a string, returns empty string", () => {
+  expect(capitalize({})).toBe('');
+  expect(capitalize([])).toBe('');
+  expect(capitalize(242342)).toBe('');
+});
+
+test("if the argument is empty, returns empty string", () => {
+  expect(capitalize()).toBe('');
 });
